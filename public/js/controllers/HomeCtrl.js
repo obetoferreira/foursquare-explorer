c5be05d8ef254e3d77ca0ceaf79d83999a89f3af
obetoferreira <obetoferreira@gmail.com>
2015-11-18 23:49:44 -0200

    Integrating mapbox with front-end and foursquare

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,32 +1,86 @@
 angular.module('HomeCtrl', []).controller('HomeController', function($scope, homeFactory, $mdBottomSheet, $mdDialog) {
 
 	$scope.category = $scope.category || 'sushi';
-	$scope.distance = $scope.distance ||  500;
+	$scope.distance = $scope.distance ||  3000;
 
 	// GeoLocation
 	navigator.geolocation.getCurrentPosition(function(position) {
-		$scope.latLng = position.coords.latitude + ',' +  position.coords.longitude;
-		$scope.getPlaces();
-		$scope.renderMap();
+		// Latitude and Longitude
+		$scope.lat = position.coords.latitude;
+		$scope.lng = position.coords.longitude;
+
+		if ( $scope.lat && $scope.lng ) {
+			// Map
+			$scope.renderMap();
+		} else {
+			alert('Please, enable your geolocation.');
+		}
 	});
 
 	// Get 
-	$scope.getPlaces = function( args ){
-		if ( !args ) {
-			args = { latLng: $scope.latLng, category: $scope.category, distance: $scope.distance }
-		}
+	$scope.getPlaces = function( callback ){
+		args = {
+			lat: $scope.lat,
+			lon: $scope.lng,
+			category: $scope.category, 
+			distance: $scope.distance
+		};
+
 		homeFactory.getPlaces(args)
 			.success(function(data){
 				$scope.places = data.response.venues;
+				callback();
 			})
 			.error(function(err){
-				$scope.status = err.message;
+				$scope.places = err.message;
 			});
 	}
 
 	// Update
-	$scope.updatePlaces = function( args ){
+	$scope.renderMarkers = function( ){
+		// Main Marker
+		$scope.map.eachLayer(function(layer) {
+		  if (layer instanceof L.Marker) {
+		    $scope.map.removeLayer(layer);
+		  }
+		});
+		L.marker([$scope.lat, $scope.lng], {
+			icon: L.mapbox.marker.icon({
+				'marker-size': 'large',
+				'marker-symbol': 'star',
+				'marker-color': '#fa0'
+			})
+		}).addTo($scope.map);
+		// Places Layers
+		$scope.getPlaces(function(){
+			if ($scope.places) {
+				for (var i = 0; i < $scope.places.length; i++) {
+					var place = $scope.places[i],
+						popupText = '';
+
+					if ( place.name ) {
+						popupText = popupText + '<h2>' + place.name + '</h2>';
+					}
+					if ( place.location.address ) {
+						popupText = popupText + '<p><b>Endereço:</b> ' + place.location.address + '</p>';
+					}
+					if ( place.contact.formattedPhone ) {
+						popupText = popupText + '<p><b>Telefone:</b> ' + place.contact.formattedPhone + '</p>';
+					}
+					if ( place.stats.checkinsCount ) {
+						popupText = popupText + '<p><i><small>Checkins: ' + place.stats.checkinsCount + '</small></i></p>';
+					}
+					console.log( place );
+					L.marker([place.location.lat, place.location.lng])
+						.bindPopup(popupText)
+						.addTo($scope.map);
+				}
+			}
+		});
+	}
+	$scope.updatePlaces = function( ){
 		$scope.hideBottomSheet();
+		$scope.renderMarkers();
 	}
 
 	// BottomSheet
@@ -45,33 +99,13 @@ angular.module('HomeCtrl', []).controller('HomeController', function($scope, hom
 
 	$scope.renderMap = function() {
 		L.mapbox.accessToken = 'pk.eyJ1Ijoib2JldG9mZXJyZWlyYSIsImEiOiJjaWgyMnFucG4weGY2dnFtMzlpZDZnaTdmIn0.BlXsoPukIgpiCxVS1ORXCg';
-		var tilejson = {
-		  "tiles": [ "https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidHJpc3RlbiIsImEiOiJuZ2E5MG5BIn0.39lpfFC5Nxyqck1qbTNquQ" ],
-		  "minzoom": 0,
-		  "maxzoom": 18
-		}
-		L.mapbox.map('map', tilejson, {
-		    scrollWheelZoom: false
-		}).setView([-19.9190936,-44.0012427], 15);
+		// map
+		$scope.map = L.mapbox.map('map', 'mapbox.streets')
+			.setView([$scope.lat, $scope.lng], 15);
+		// Disable wheel Scroll
+		$scope.map.scrollWheelZoom.disable();
+		// Markers
+		$scope.renderMarkers();
 	};
-
-	$scope.showDialog = function($event) {
-       var parentEl = angular.element(document.body);
-       $mdDialog.show({
-         template:
-           '<md-dialog aria-label="List dialog">' +
-           '  <md-dialog-actions>' +
-           '    <md-button ng-click="closeDialog()" class="md-primary">' +
-           '      Close Dialog' +
-           '    </md-button>' +
-           '  </md-dialog-actions>' +
-           '</md-dialog>',
-           controller: 'HomeController'
-      });
-	}
-
-	$scope.closeDialog = function() {
-        $mdDialog.hide();
-    }
 
 });
--- public/js/services/HomeService.js
+++ public/js/services/HomeService.js
@@ -5,7 +5,7 @@ angular.module('HomeService', []).factory('homeFactory', ['$http', function($htt
 		clientSecret = 'WQBBBHFAXTXLMFYWCESJWHNFPZWVR3EXO01NBABG3O0YWH1S';
 
 	dataFactory.getPlaces = function(args) {
-		urlBase = 'https://api.foursquare.com/v2/venues/search?client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20130815&ll=' + args.latLng + '&query=' + args.category + '&radius=' + args.distance;
+		urlBase = 'https://api.foursquare.com/v2/venues/search?client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20130815&ll=' + args.lat + ',' + args.lon + '&query=' + args.category + '&radius=' + args.distance;
 		return $http.get(urlBase);
 	}
 
